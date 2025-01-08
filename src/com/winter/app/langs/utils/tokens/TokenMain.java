dd8b4c2c74a16bb9f071932fcb79dbf64a538fb8
erica-co <qkdxks0525@naver.com>
2025-01-08 10:10:03 +0900

    1.8

---
+++ src/com/winter/app/langs/utils/tokens/TokenMain.java
@@ -0,0 +1,43 @@
+package com.winter.app.langs.utils.tokens;
+
+import java.util.StringTokenizer;
+
+public class TokenMain {
+
+	public static void main(String[] args) {
+		// 클래스 담을 변수 선언
+		// 같은 패키지내에 있는가 또는 java.lang 에 있는가 찾기
+		// import 안하고 사용가능
+		
+		// 하나의 문자열을 여러개의 문자열로 분리 : parsing
+		// 분리한 문자열 하나하나를 Token이라고 부름
+		
+		String data="iu-173.2-59.3";
+		StringTokenizer st = new StringTokenizer(data,"-"); //(분리하고자 하는 것, 분리하는 기준)
+		int count = st.countTokens();
+		System.out.println(count);
+		
+		boolean check = st.hasMoreTokens();
+		
+		while(st.hasMoreTokens()) {
+				
+				String t = st.nextToken();
+				System.out.println(t);
+				
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- src/com/winter/app/langs/wrapper/Wrapper4.java
+++ src/com/winter/app/langs/wrapper/Wrapper4.java
@@ -4,15 +4,18 @@ public class Wrapper4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		//10진수
 		int num = 1;
 		
+		//2진수
 		String str = Integer.toBinaryString(num); //0~9
 		System.out.println(str);
 		
+		//16진수
 		str = Integer.toHexString(num); //0~15 /10~15 abcdef로 표시됨
 		System.out.println(str);
 		
+		//8진수
 		str = Integer.toOctalString(num);
 		System.out.println(str);
 	}
